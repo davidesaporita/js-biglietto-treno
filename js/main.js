8586f099aa4ddc9138570e60d8290d40a5ec2bd8
Davide Saporita <davide.saporita@gmail.com>
2020-03-26 16:22:01 +0100

    JS | Sending data to HTML --- CSS | Ticket style (draft)

--- js/main.js
+++ js/main.js
@@ -6,22 +6,30 @@
  */
 
 // 1) Inizializzazione variabili e raccolta dati
+
 var kmPercorrenza = parseInt(prompt('Allora sentiamo, quanti chilometri devi percorrere?'));
 alert('Non ti sembrano un po\' troppi alla tua età?');
 
 var etaPasseggero = parseInt(prompt('A proposito, quanti anni hai?'));
 alert('Clicca OK se hai appena dichiarato il falso');
 
+var sconto = 0;
+
 // 2) Calcolo del prezzo del biglietto standard (0,21€/km)
+
 var prezzoStandard = (kmPercorrenza * 0.21).toPrecision(3);
 
 // 3) Verifica delle condizioni per l'applicazione dello sconto (-20% per <18 e -40% per >= 65) e calcolo degli importi scontati
+
 if ( etaPasseggero < 18) {
-    prezzoScontato = (prezzoStandard * 0.80).toPrecision(3);
-    alert('Compare ma sei minorenne! Ti becchi lo sconto del 20% e paghi solo € ' + prezzoScontato + ' | Ora clicca OK per avvisare automaticamente i tuoi genitori.');
+    prezzoFinale = (prezzoStandard * 0.80).toPrecision(3);
+    sconto = 20;
 } else if ( etaPasseggero >= 65 ) {
-    prezzoScontato = (prezzoStandard * 0.60).toPrecision(3);
-    alert('Grande Nonno! Tu hai il 40% di sconto. Si, non ti preoccupare il calcolo lo faccio io, devi sborsare € ' + prezzoScontato + ' | Aspetta, dove vai? Hai lasciato qui la dentiera!');
+    prezzoFinale = (prezzoStandard * 0.60).toPrecision(3);
+    sconto = 40;    
 } else {
-    alert('Vabbé tu paghi prezzo pieno, ovvero la modica cifra di €' + prezzoStandard );
+    prezzoFinale = prezzoStandard;
 }
+
+// 4) Inserimento dei valori ottenuti in HTML
+document.getElementById('prezzo').innerHTML = prezzoFinale + ' <br>Complimenti! Stai anche usufruendo<br>del seguente sconto: ' + sconto + '%!';
